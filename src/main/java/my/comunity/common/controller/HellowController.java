a49a60320290e9592b0288185398535d577e6790
zhangxu23 <289335640@qq.com>
2019-11-21 16:56:21 +0800

    init

---
+++ src/main/java/my/comunity/common/controller/HellowController.java
@@ -0,0 +1,15 @@
+package my.comunity.common.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HellowController {
+    @GetMapping("hellow")
+    public String hellow(@RequestParam("name")String name, Model model) {
+        model.addAttribute("name",name);
+        return "hellow";
+    }
+}
